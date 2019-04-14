65abb9fa144a04aa40a8c2f2475b4f19d0210301
RubenFonsecaKzn <49552055+RubenFonsecaKzn@users.noreply.github.com>
2019-04-14 18:17:33 +0200

    Practica1Completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
-@NgModule({
+
+@NgModule( {
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
-})
+} )
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
-import { FooterComponent } from './footer/footer.component';
+import { NgModule } from '@angular/core';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
 import { LayoutComponent } from './layout/layout.component';
+import { MainComponent } from './layout/main/main.component';
 
-@NgModule({
-  declarations: [HeaderComponent, MainComponent, FooterComponent, LayoutComponent],
+@NgModule( {
+  declarations: [LayoutComponent, HeaderComponent, MainComponent, FooterComponent],
   imports: [
     CommonModule
-  ]
-})
+  ],
+  exports: [LayoutComponent]
+} )
 export class CoreModule { }
